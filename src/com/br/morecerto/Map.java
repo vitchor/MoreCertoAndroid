0f317ed47261a7dc02629d6b568c04eba727d3f9
Victor <vitchor@gmail.com>
2012-05-19 12:29:47 -0300

    Initial commit

---
+++ src/com/br/morecerto/Map.java
@@ -0,0 +1,20 @@
+package com.br.morecerto;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+
+public class Map extends MapActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
