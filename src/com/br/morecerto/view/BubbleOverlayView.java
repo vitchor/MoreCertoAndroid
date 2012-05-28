1e664925fba39cd250bd813a8c21dd785b3bcff3
Victor <vitchor@gmail.com>
2012-05-28 00:09:39 -0300

    Added spinner to map popups

--- src/com/br/morecerto/view/BubbleOverlayView.java
+++ src/com/br/morecerto/view/BubbleOverlayView.java
@@ -2,12 +2,16 @@ package com.br.morecerto.view;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.br.morecerto.R;
@@ -16,18 +20,19 @@ import com.br.morecerto.controller.network.Request;
 import com.br.morecerto.controller.network.Response;
 import com.google.android.maps.OverlayItem;
 
-public class BubbleOverlayView<Item extends OverlayItem> extends FrameLayout implements OnDownloadListener{
+public class BubbleOverlayView<Item extends OverlayItem> extends FrameLayout implements OnDownloadListener {
 
 	private TextView mTitle;
 	private TextView mSubtitle;
 	private ImageView mImage;
+	private ProgressBar mSpinner;
 
 	public BubbleOverlayView(Context context, boolean isClickable) {
 		super(context);
 		final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View view;
 		LinearLayout layout = new LinearLayout(context);
-		
+
 		if (isClickable) {
 			view = inflater.inflate(R.layout.bubble_clickable_overlay, layout);
 		} else {
@@ -37,11 +42,12 @@ public class BubbleOverlayView<Item extends OverlayItem> extends FrameLayout imp
 		params.gravity = Gravity.NO_GRAVITY;
 		layout.setVisibility(VISIBLE);
 		addView(layout, params);
-		
+
 		mTitle = (TextView) view.findViewById(R.id.bubble_title);
 		mSubtitle = (TextView) view.findViewById(R.id.bubble_subtitle);
 		mImage = (ImageView) view.findViewById(R.id.image_view);
-		
+		mSpinner = (ProgressBar) view.findViewById(R.id.spinner);
+		mSpinner.bringToFront();
 		setClickable(isClickable);
 	}
 
@@ -49,7 +55,7 @@ public class BubbleOverlayView<Item extends OverlayItem> extends FrameLayout imp
 		mTitle.setVisibility(VISIBLE);
 		mTitle.setText(title);
 	}
-	
+
 	public void setSubtitle(String subtitle) {
 		mSubtitle.setVisibility(VISIBLE);
 		mSubtitle.setText(subtitle);
@@ -57,29 +63,34 @@ public class BubbleOverlayView<Item extends OverlayItem> extends FrameLayout imp
 
 	@Override
 	public void onPreLoad(int type) {
-		// TODO Auto-generated method stub
-		
+		mSpinner.setVisibility(View.VISIBLE);
+		ColorDrawable color = new ColorDrawable(Color.BLACK);
+		mImage.setImageDrawable(color);
+
 	}
 
 	@Override
 	public void onLoad(Response response) {
-		
+		mSpinner.setVisibility(View.GONE);
+
 		Bitmap image = response.getBitmap();
-		if (image != null){
+		if (image != null) {
 			mImage.setImageBitmap(image);
+		} else {
+			mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_home));
 		}
-		
+
 	}
 
 	@Override
 	public void onError(int type, Request request, Exception exception) {
-		// TODO Auto-generated method stub
-		
+		mSpinner.setVisibility(View.GONE);
+		mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_home));
 	}
 
 	@Override
 	public void onCancel() {
-		// TODO Auto-generated method stub
-		
+		mSpinner.setVisibility(View.GONE);
+		mImage.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_home));
 	}
 }
